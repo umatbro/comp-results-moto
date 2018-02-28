eeb8e12d06b7a4be03672ee6bea56526e4e04952
umatbro <umatbroo@gmail.com>
2018-02-28 14:39:12 +0100

    create controllers dir

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('respond with a resource');
+});
+
+router.get('/cool', (req, res) => {
+  res.send('You cool');
 });
 
 module.exports = router;
