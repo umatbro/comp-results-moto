53a4be5521ef88391b81186474ff6b659330ac47
umatbro <umatbroo@gmail.com>
2018-02-28 22:58:33 +0100

    eslint with google style

--- test/test_routes.js
+++ test/test_routes.js
@@ -1,12 +1,11 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
 const request = require('supertest');
-const express = require('express');
 const app = require('../app');
 
 
 describe('Routing', () => {
-  before(done => {  // run test server
+  before((done) => { // run test server
     this.app = app;
     let port = Math.floor(Math.random() * 9000) + 1000;
     this.server = this.app.listen(port, () => done());
@@ -23,15 +22,15 @@ describe('Routing', () => {
 
     afterEach( () => this.spy.restore() );
 
-    it(' \'/\' should render index template', done => {
+    it(' \'/\' should render index template', (done) => {
       request(this.app).get('/').expect(200).end((err, res) => {
-        let [view, locals, callback] = this.spy.getCall(0).args;
-        expect(view).to.equal('index');
+        let templateName = this.spy.getCall(0).args[0];
+        expect(templateName).to.equal('index');
         done();
       });
     });
 
-    it('\'/admin\' sould render admin template', done => {
+    it('\'/admin\' sould render admin template', (done) => {
       request(this.app)
         .get('/admin')
         .expect(200)
