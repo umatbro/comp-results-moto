27d1804219dd9378dbe577e220be34ce751bbac9
umatbro <umatbroo@gmail.com>
2018-03-17 20:34:55 +0100

    add function generating random tracks, closes #7

--- utils.js
+++ utils.js
@@ -10,17 +10,6 @@ String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 };
 
-commandDescription =
-`This command will store 100 random users in your database using https://randomuser.me/ API.
-
-Example usage:
-./load_users.js 100 --db=ds261078.mlab.com:61078/comp-results-test -u <dbuser> -p <dbpassword>`;
-
-const parser = new ArgumentParser({
-  addHelp: true,
-  description: commandDescription,
-});
-
 function loadRandomUsers(dbURI, numOfUsers=100) {
     mongoose.connect(dbURI);
     fetch('https://randomuser.me/api/?results=' + numOfUsers)
@@ -30,6 +19,40 @@ function loadRandomUsers(dbURI, numOfUsers=100) {
                 let name = user.name;
                 return Contestant.create({
                     name: name.first.capitalize() +' '+ name.last.capitalize(),
+                });
+            })
+        ))
+        .then((res) => console.log('Number of records:', res.length))
+        .catch((err) => console.log(err))
+        .then(() => mongoose.connection.close());
+}
+
+/**
+ * Get point score for a track based on its length
+ * and random difficulty factor.
+ *
+ * @param {Number} length
+ * @return {number} point score
+ */
+function lengthToPoints(length) {
+    // difficulty: random range 0.5 - 1.5
+    let difficultyFactor = Math.random() + 0.5;
+    return ~~(length * difficultyFactor * 0.01);
+}
+
+function generateRandomTracks(dbURI, numOfTracks=30) {
+    mongoose.connect(dbURI);
+    fetch('https://randomuser.me/api/?results=' + numOfTracks)
+        .then((res) => res.json())
+        .then((response) => Promise.all(
+            response.results.map((track) => {
+                let location = track.location;
+                // lengths from 500 - 10 000 m
+                let trackLength = Math.floor(Math.random() * 9500 + 500);
+                return Track.create({
+                    name: location.city.capitalize(),
+                    length: trackLength,
+                    points: lengthToPoints(trackLength),
                 });
             })
         ))
@@ -55,6 +78,7 @@ function httpResReqMocks(responseOptions={}, requestOptions={}) {
 module.exports = {
     httpMocks: httpResReqMocks,
     generateRandomUsers: loadRandomUsers,
+    generateRandomTracks: generateRandomTracks,
 };
 
 if (!module.parent) {
